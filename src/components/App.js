bea558574a323abb188c26f1c5702bd0904ffb11
Jack Pena <penajackc@gmail.com>
2022-02-04 09:36:00 -0700

    rendering pigs

--- src/components/App.js
+++ src/components/App.js
@@ -7,8 +7,32 @@ function App() {
 	return (
 		<div className="App">
 			<Nav />
+			<Card />
+			
 		</div>
 	);
 }
 
 export default App;
+
+const eachHogCard = hogs.map(hog => {
+	return (
+		<div className="ui eight wide column">
+			<img src={hog.image}/>
+			<h2>{hog.name}</h2>
+		</div>
+	)
+})
+
+function Card(){
+
+	const eachHogCard = hogs.map(hog => {
+		return (
+			<div className="ui eight wide column">
+				<img src={hog.image}/>
+				<h2>{hog.name}</h2>
+			</div>
+		)
+	})
+	return eachHogCard
+}
