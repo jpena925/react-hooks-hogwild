0a39cc73af4388dbdd94ed545d5691dc4855ebd8
Danielle Bencze <daniellebencze@live.com>
2022-02-04 10:41:51 -0800

    latest commit

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,36 +1,49 @@
 import React, { useState } from "react";
-import HogCard from "./HogCard"
+import HogCard from "./HogCard";
 
 function CardList({ hogs }) {
-  const [showGreased, setShowGreased] = useState(true)
-  const [hogsToShow, setHogsToShow] = useState(hogs)
+  const [showGreased, setShowGreased] = useState(true);
+  const [hogsToShow, setHogsToShow] = useState(hogs);
 
-  function handleGreaseFilter(){
-    if(showGreased){
-    setHogsToShow(() => hogsToShow
-      .filter(hog => hog.greased === showGreased))
+  function handleGreaseFilter() {
+    if (showGreased) {
+      setHogsToShow(() =>
+        hogsToShow.filter((hog) => hog.greased === showGreased)
+      );
     } else {
-      setHogsToShow(() => hogs)
+      setHogsToShow(() => hogs);
     }
-    setShowGreased(() => !showGreased)
+    setShowGreased(() => !showGreased);
   }
 
+  function handleSort(e) {
+    console.log(e.target.value);
+  }
 
   return (
     <>
       <div>
-        <input type="checkbox" id="greasebox" onChange={handleGreaseFilter}/>
-          <label htmlFor="greased">Greased Hogs</label>
+        <input type="checkbox" id="greasebox" onChange={handleGreaseFilter} />
+        <label htmlFor="greased">Greased Hogs</label>
+        <br></br>
+        <br></br>
+        <select onChange={handleSort}>
+          <option disabled selected>
+            Sort by...
+          </option>
+          <option>Name</option>
+          <option>Weight</option>
+        </select>
         <br></br>
         <br></br>
       </div>
-      <div className = "ui three stackable cards">
+      <div className="ui three stackable cards">
         {hogsToShow.map((hog) => (
           <HogCard key={hog.name} hog={hog} />
         ))}
       </div>
     </>
-    );
-  }
+  );
+}
 
-export default CardList
+export default CardList;
